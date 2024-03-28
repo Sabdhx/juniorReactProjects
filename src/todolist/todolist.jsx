f068fd13998de45a77de1f95052ccf49c723f703
Sabdhx <syedaliabdullahshah5@gmail.com>
2024-03-28 18:01:50 +0500

    completed

--- src/todolist/todolist.jsx
+++ src/todolist/todolist.jsx
@@ -20,6 +20,14 @@ const Todolist = () => {
     
   }
   const navigate = useNavigate();
+
+
+  const handledelete = (id)=>{
+ 
+ setTodoItems(
+   todoItems.filter((item)=>item.id!==id)
+ )
+  }
   return (
     <div className=" mx-auto bg-white shadow-md overflow-hidden sm:rounded-lg mt-[100px]">
       <div className="p-4 bg-gray-100">
@@ -51,7 +59,9 @@ const Todolist = () => {
                   update
                 </button>
 
-                <button className="ml-2 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                <button
+                onClick={()=>handledelete(item.id)}
+                className="ml-2 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                   remove
                 </button>
               </div>
